afcab0f5fb4c45e9f4482c96d5003dfcf93ce3b1
Doniel Tripura <donieltripura@icloud.com>
2025-10-04 18:11:18 +0600

    Initial Commit

---
+++ TTS/TTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTSApp.swift
+//  TTS
+//
+//  Created by Doniel Tripura on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
